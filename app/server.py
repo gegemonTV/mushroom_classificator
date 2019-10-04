caefdbc631529eb110bbd60e4eaeebcdf7680e87
gegemonTV <51519538+gegemonTV@users.noreply.github.com>
2019-10-05 01:04:21 +0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1-SRFTxeYn6Z-kjfr9BFO0UQFI7MZKMbP?alt=media&key=AIzaSyD_KIRkgVk-YohOs6SmD84nfbl8MIFDwgo'
+export_file_name = 'stage-1.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['brown_boletus', 'ginger', 'oc_boletus', 'porcini', 'chanterelle']
 path = Path(__file__).parent
 
 app = Starlette()
